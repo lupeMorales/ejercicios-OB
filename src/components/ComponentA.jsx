8e4605bcdef4aa5aec15c78df24cb1fb65a4f81f
lupeMorales <morales.lupegmail.com>
2022-10-15 17:27:58 +0200

    add exercise 1-2-3

---
+++ src/components/ComponentA.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Contact } from "../models/contact.class";
+import ComponentB from "./ComponentB";
+
+const ComponentA = () => {
+  const defaultContact = new Contact("name", "surname", "mail@mail.com", false);
+  return (
+    <div>
+      <ComponentB contact={defaultContact} />
+    </div>
+  );
+};
+
+export default ComponentA;
